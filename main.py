be8c295d9d6c7ba273603e16cf768e2c2b195703
Francisco Almeida <1161130@isep.ipp.pt>
2019-06-19 17:38:47 +0100

    now appends each request's time to a file

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import sys
 URL = sys.argv[1]
 TOKEN = sys.argv[2]
 NUMBER_REQUESTS = 100
+OUTPUT_FILE = 'results.txt'
 sum = 0
+output_file = open(OUTPUT_FILE, 'a+')
 
 for i in range(0, NUMBER_REQUESTS):
   time = requests.get(URL, cookies = {
     'token': TOKEN
   }).elapsed.total_seconds()
+
+  output_file.write('%f\r\n' % time)
 
   time_ms = time * 1000
 
@@ -20,3 +24,5 @@ for i in range(0, NUMBER_REQUESTS):
 average = (sum / NUMBER_REQUESTS) * 1000
 
 print('Avg. request time: {0}ms'.format(int(average)))
+
+output_file.close()
